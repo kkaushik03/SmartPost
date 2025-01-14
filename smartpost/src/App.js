72fee8f201aee5cafc1660f57fee4716469ab1c9
kkaushik03 <111904079+kkaushik03@users.noreply.github.com>
2025-01-14 09:51:01 -0800

    Hello World Frontend

---
+++ smartpost/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello World
+        </p>
+        {/* Add A Form Later*/}
+      </header>
+    </div>
+  );
+}
+
+export default App;
