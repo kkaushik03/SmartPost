defcb555fac9f2a3335bcc02edb1e548ce5e073b
Ally <90171984+Allyfaith@users.noreply.github.com>
2025-02-13 19:52:44 -0800

    Update Features.jsx
    side by side fixed

--- smartpost/src/components/LandingPage/Features/Features.jsx
+++ smartpost/src/components/LandingPage/Features/Features.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import "./features.css";
 import Feature1 from "../../../assets/Features/Feature_1.png";
@@ -7,41 +8,43 @@ const Features = () => {
   return (
     <div id="features" className="featurecontainer">
       <div className="ZoneOne">
-        <div className="AiFeatureOne">
-          <p>AI Features</p>
+        <div className="feature-content">
+          <div className="AiFeatureOne">
+            <p>AI Features</p>
+          </div>
+          <div className="FeatureTitleOne">Title</div>
+          <div className="FeatureDescriptionOne">Description</div>
+          <div className="FeatureListOne">
+            <ul>
+              <li>Feature 1</li>
+              <li>Feature 2</li>
+              <li>Feature 3</li>
+            </ul>
+          </div>
         </div>
-        <div className="FeatureTitleOne">Title</div>
-        <div className="FeatureDescriptionOne">Description</div>
-        <div className="FeatureListOne">
-          <ul>
-            <li>Feature 1</li>
-            <li>Feature 2</li>
-            <li>Feature 3</li>
-          </ul>
-        </div>
-
         <div className="ImageFeatureOne">
           <img src={Feature1} alt="Feature 1" />
         </div>
       </div>
 
       <div className="ZoneTwo">
-        <div className="AiFeatureTwo">
-          <p>AI Features</p>
+        <div className="feature-content">
+          <div className="AiFeatureTwo">
+            <p>AI Features</p>
+          </div>
+          <div className="FeatureTitleTwo">Seamless Follow-up Questions</div>
+          <div className="FeatureDescriptionTwo">
+            Keeps The Conversation Flowing Naturally By Asking Relevant Follow-Up
+            Questions.
+          </div>
+          <div className="FeatureListTwo">
+            <ul>
+              <li>Feature 1</li>
+              <li>Feature 2</li>
+              <li>Feature 3</li>
+            </ul>
+          </div>
         </div>
-        <div className="FeatureTitleTwo">Seamless Follow-up Questions</div>
-        <div className="FeatureDescriptionTwo">
-          Keeps The Conversation Flowing Naturally By Asking Relevant Follow-Up
-          Questions.
-        </div>
-        <div className="FeatureListTwo">
-          <ul>
-            <li>Feature 1</li>
-            <li>Feature 2</li>
-            <li>Feature 3</li>
-          </ul>
-        </div>
-
         <div className="ImageFeatureTwo">
           <img src={Feature2} alt="Feature 2" />
         </div>
