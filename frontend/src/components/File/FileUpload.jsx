1d69402f27021c2644f4ce5184455f5f80858951
Ngoc Nguyen <ngocnguyen@Ngocs-Air.lan>
2025-03-09 15:53:06 -0700

    connect frontend with backend server

--- frontend/src/components/File/FileUpload.jsx
+++ frontend/src/components/File/FileUpload.jsx
@@ -106,7 +106,7 @@ Allowed types are: ${allowedTypes.join(', ')}.`;
     }, 500);
 
     try {
-      const response = await fetch("http://127.0.0.1/upload", {
+      const response = await fetch("https://codetech-k96i.onrender.com/upload", {
         method: "POST",
         body: formData,
       });
